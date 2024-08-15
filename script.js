3ed7823570337e86cb6687f9e55c66be184506ca
Sharul23 <SHARULGAUTAM72@GMAIL.COM>
2024-08-15 15:52:42 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -11,3 +11,32 @@ if(close) {
         nav.classList.remove('active');
     })
 }
+
+//cart logic
+
+document.addEventListener('DOMContentLoaded', () => {
+    const cart = JSON.parse(localStorage.getItem('cart')) || [];
+
+    document.querySelectorAll('.add-to-cart').forEach(button => {
+        button.addEventListener('click', () => {
+            const product = button.getAttribute('data-product');
+            const price = parseFloat(button.getAttribute('data-price'));
+            const image = button.getAttribute('data-image');
+            const existingProductIndex = cart.findIndex(item => item.product === product && item.image === image);
+
+            if (existingProductIndex > -1) {
+                // Update quantity if product already exists in the cart
+                cart[existingProductIndex].quantity += 1;
+            } else {
+                // Add new product to the cart
+                cart.push({ product, price, quantity: 1, image });
+            }
+
+            localStorage.setItem('cart', JSON.stringify(cart));
+            alert('Product added to cart!');
+        });
+    });
+});
+
+
+
